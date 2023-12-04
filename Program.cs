fefba5754917b4e8f7945e1e46116f220057b693
Camilo Machado Francisco <camilomachado@live.com>
2023-12-04 11:38:58 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using ExerciciosCSharp;
+
+namespace ExerciciosCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", Exercicio1.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
