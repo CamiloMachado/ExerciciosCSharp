dafbfc62e551a3d3281818c30e5d6d5e44077d92
Camilo Machado Francisco <camilomachado@live.com>
2023-12-04 12:26:28 -0300

    Solucionado exercício 1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using ExerciciosCSharp;
+using ExerciciosCSharp.Exercicios;
 
 namespace ExerciciosCSharp {
     class Program {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
-                {"Primeiro Programa - Fundamentos", Exercicio1.Executar},
+
+                // Exercícios
+                {"Exercicio #01", Exercicio1.Executar},
+                {"Exercicio #02", Exercicio2.Executar},
             });
 
             central.SelecionarEExecutar();
