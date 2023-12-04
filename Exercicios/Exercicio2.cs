b920d2896b0cce63018b051be7886386b8acf91e
Camilo Machado Francisco <camilomachado@live.com>
2023-12-04 12:35:45 -0300

    Solucionado exercício 2

---
+++ Exercicios/Exercicio2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosCSharp.Exercicios {
+
+    // Faça um programa que: 
+    // - Leia a cotação do dólar 
+    // - Leia um valor em dólares 
+    // - Converta esse valor para Real 
+    // - Mostre o resultado
+
+    internal class Exercicio2 {
+
+        public static void Executar() {
+            
+            double cotacaoDolar = 0;
+            double valorDolar = 0;
+            double valorEmReal = 0;
+
+            valorEmReal = cotacaoDolar + valorEmReal;
+
+            Console.WriteLine($"Cotação do dólar: {cotacaoDolar}");
+            Console.WriteLine($"Valor em dólares: {valorDolar}");
+            Console.WriteLine($"Valor em reais {valorEmReal}");
+        }
+    }
+}
